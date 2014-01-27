8ad0dfb38a0c98758a6d2735d4019b0f5e2b2aa3
Terry.Cho <bwcho75@gmail.com>
2014-01-28 02:21:57 +0900

    Update and rename eventbus_reply_with_httpreponse to eventbus_reply_with_httpreponse.py

--- simple_http_server/eventbus_reply_with_httpreponse.py
+++ simple_http_server/eventbus_reply_with_httpreponse.py
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 # usage http://localhost:8080/eventbus/hello
+# all event bus message handler and reply handler also executed in event loop thread
 import vertx
+import time
 from core.http import RouteMatcher
 from core.event_bus import EventBus
 
@@ -9,23 +11,29 @@ server = vertx.create_http_server()
 route_matcher = RouteMatcher()
 
 
-def message_handler(message):
+def message_handler(message): # this is executed in event loop
     print 'Im worker and i got a message :'+str(message.body)
+    time.sleep(5)
     # do some logic for data base transaction
     # populate return message
     message.reply('This is reply for message :'+str(message.body))
 
 def reply_handler_logic(req,message):
+	print 'im reply handler, i just invoked'
+   	time.sleep(5)
 	key = req.params['key']
    	req.response.end('event bus call is called and replied from reply_handler :'+ key +str(message.body))
         
 EventBus.register_handler('call_bus',handler=message_handler)
 
-def url_handler(req): 
+def url_handler(req): # this is invoked by eventloop thread
         # read parameter from URI
         key = req.params['key']
         def reply_handler(message):
         	reply_handler_logic(req,message)
+        print 'im url handler i will send message to event bus'
+       	time.sleep(5)
+       	print 'Im url handler. I just sent message to event bus'
         EventBus.send('call_bus',key,reply_handler)
                 
 route_matcher.get('/eventbus/:key',url_handler)
